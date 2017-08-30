64ce6d63a6d2e9010143b5283d16ab75f5dc7e45
piyusharma <piyush.sharma301@gmail.com>
2017-08-30 16:25:23 +0530

    TimeFormat moved and removed

--- src/MainThreadHOC.java
+++ src/MainThreadHOC.java
@@ -159,4 +159,24 @@ public class MainThreadHOC {
             this.timeFormat = timeFormat;
         }
     }
+
+    public class TimeFormat {
+        private int seconds;
+        private int minutes;
+        private int hours;
+        private int days;
+
+        TimeFormat(int seconds,int minutes,int hours,int days){
+            this.seconds = seconds;
+            this.minutes = minutes;
+            this.hours = hours;
+            this.days = days;
+        }
+
+        @Override
+        public String toString() {
+            return days+" days "+hours+" hours "+minutes+" minutes "+seconds+" seconds ";
+        }
+    }
+
 }
